5c0f5c503d7de5fde8f784a2c07b887c7a60bc53
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-05-18 01:15:41 +0530

    Completed Exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "040-1234567", id: 1 },
+        { name: "Arto Hellas", number: "040-123456" },
+        { name: "Ada Lovelace", number: "39-44-5323523" },
+        { name: "Dan Abramov", number: "12-43-234345" },
+        { name: "Mary Poppendieck", number: "39-23-6423122" },
     ]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
