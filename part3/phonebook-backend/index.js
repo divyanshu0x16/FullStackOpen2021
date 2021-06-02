0967c4449a8ca4e73c7f49d24fa572887fee40a1
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-06-02 17:42:49 +0530

    MongoDB Delete (Exercise 3.15)

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,38 +1,15 @@
-require('dotenv').config()
+require("dotenv").config();
 const { request, response } = require("express");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
-const Person = require('./models/person')
+const Person = require("./models/person");
 
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.static("build"));
-
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1,
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2,
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3,
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4,
-    },
-];
 
 app.get("/api/persons", (request, response) => {
     Person.find({}).then((persons) => {
@@ -52,17 +29,18 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = (request.params.id);
-    Person.find({ _id : id}).then((person) => {
-        response.json(person)
+    const id = request.params.id;
+    Person.find({ _id: id }).then((person) => {
+        response.json(person);
     });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter((person) => person.id !== id);
-
-    response.status(204).end();
+    Person.findByIdAndRemove(request.params.id)
+        .then((result) => {
+            response.status(204).end();
+        })
+        .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -93,11 +71,11 @@ app.post("/api/persons", (request, response) => {
         number: body.number,
     });
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save().then((savedPerson) => {
+        response.json(savedPerson);
+    });
 });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
