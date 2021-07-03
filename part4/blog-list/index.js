d3a590fdb558dcf8be574bae91f382430d5de060
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-03 23:49:14 +0530

    completed till 4.2

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,29 +1,16 @@
-require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const Blog = require('./models/blogs');
+const Blog = require('./models/blog');
+const logger = require('./utils/logger');
+const config = require('./utils/config');
+const blogsRouter = require('./controllers/blogs');
 
 app.use(cors());
 app.use(express.json()); //for the body property of the object
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+app.use('/api', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
