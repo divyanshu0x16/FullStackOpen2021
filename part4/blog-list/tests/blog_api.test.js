01b7c85aff118cbb74dd62a3dd3a755fb4a23a71
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-05 18:35:02 +0530

    added json length test

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -1,11 +1,36 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
-const app = require('../index');
-
+const app = require('../app');
+const Blog = require('../models/blog');
 const api = supertest(app);
 
+const initialBlogs = [
+  {
+    title: 'lal laal al',
+    author: 'me!!!',
+    url: 'goodle',
+    likes: 0,
+  },
+  {
+    title: 'lol',
+    author: 'adfaf',
+    url: 'duckfuckno',
+    likes: 1000,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+});
+
 test('blog list application return correct amount of posts in JSON', async () => {
-  await api.get('/api/notes');
+  const response = await api.get('/api/blogs');
+
+  expect(response.body).toHaveLength(initialBlogs.length);
 });
 
 afterAll(() => {
