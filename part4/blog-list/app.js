732867085d1e3c4d3881ed1f3109a699035ccb5d
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-06 18:49:53 +0530

    added token middleware, completed 4.20

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
-require('express-async-errors')
+require('express-async-errors');
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
 
-const middleware = require('./utils/middleware')
+const middleware = require('./utils/middleware');
 
 app.use(cors());
 app.use(express.json()); //for the body property of the object
+
+app.use(middleware.tokenExtractor);
 
 app.use('/api', blogsRouter);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
 
-app.use(middleware.errorHandler)
+app.use(middleware.errorHandler);
 
 module.exports = app;
