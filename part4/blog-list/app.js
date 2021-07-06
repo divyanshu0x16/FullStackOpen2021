2c3ed6b9d8c6a198e778cb437cfb982dca90312e
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-06 11:45:17 +0530

    added user login without test

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
+const userRouter = require('./controllers/users');
 
 app.use(cors());
 app.use(express.json()); //for the body property of the object
 
 app.use('/api', blogsRouter)
+app.use('/api/users', userRouter)
 
 module.exports = app
---
+++ part4/blog-list/controllers/users.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const userRouter = require('express').Router();
+const User = require('../models/user');
+
+userRouter.post('/', async (request, response) => {
+  const body = request.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  });
+
+  try {
+    const savedUser = await user.save();
+    response.json(savedUser);
+  } catch (error) {
+    response.status(400).json(error.message);
+  }
+});
+
+userRouter.get('/', async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
+module.exports = userRouter;
