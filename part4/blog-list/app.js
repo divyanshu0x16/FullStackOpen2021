bc341c8e1c8cb2c7cfbb6e9d9f84eb54d264b852
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-06 18:07:19 +0530

    completed 4.18, added token authentication

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -3,11 +3,13 @@ const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 app.use(cors());
 app.use(express.json()); //for the body property of the object
 
-app.use('/api', blogsRouter)
-app.use('/api/users', userRouter)
+app.use('/api', blogsRouter);
+app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
-module.exports = app
+module.exports = app;
