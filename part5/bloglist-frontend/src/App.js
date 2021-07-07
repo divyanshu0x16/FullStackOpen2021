f56116c633217433b7218296fa95ff35fa33fa64
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-07 12:10:53 +0530

    completed 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -20,6 +20,15 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      //blogService.setToken(user.token);
+    }
+  }, []);
+
   const handleLogin = async (event) => {
     event.preventDefault();
 
@@ -28,6 +37,7 @@ const App = () => {
         username,
         password,
       });
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user));
       setUser(user);
       setUsername('');
       setPassword('');
@@ -69,7 +79,16 @@ const App = () => {
   const blogList = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.username} is logged in</p>
+      <span>{user.username} is logged in</span>
+      <button
+        onClick={() => {
+          window.localStorage.removeItem('loggedNoteappUser');
+          setUser(null);
+        }}
+      >
+        logout
+      </button>
+      <p></p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
