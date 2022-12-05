7cf559586b96ef3051fe77eaf49a1b6fc58a4561
divyanshu0x16 <divyanshu22.dm@gmail.com>
2022-12-05 23:23:26 +0530

    exercise 9.5

--- part9/exercises/bmiCalculator.ts
+++ part9/exercises/bmiCalculator.ts
@@ -17,27 +17,26 @@ const parseArguments = (args: Array<string>): BMIValues => {
   }
 };
 
-const bmiCalculator = (weight: number, height: number) => {
-  
-  if( weight <= 0 || height <= 0 )
-    throw new Error('Weight or Height cannot be negative')
+const bmiCalculator = (weight: number, height: number): string => {
+  if (weight <= 0 || height <= 0)
+    throw new Error('Weight or Height cannot be negative');
 
   let bmi: number = (weight * 100 * 100) / (height * height);
 
-  if (bmi < 16) console.log('Underweight (Severe thinness)');
-  else if (16 <= bmi && bmi <= 16.9)
-    console.log('Underweight (Moderate thinness)');
-  else if (17 <= bmi && bmi <= 18.4) console.log('Underweight (Mild thinness)');
-  else if (18.5 <= bmi && bmi <= 24.9) console.log('Normal Range');
-  else if (25 <= bmi && bmi <= 29.9) console.log('Overweight (Pre-obese)');
-  else if (30 <= bmi && bmi <= 34.9) console.log('Obese (Class I)');
-  else if (25 <= bmi && bmi <= 39.9) console.log('Obese (Class II)');
-  else if (bmi <= 40) console.log('Obese (Class III)');
+  if (bmi < 16) return 'Underweight (Severe thinness)';
+  else if (16 <= bmi && bmi <= 16.9) return 'Underweight (Moderate thinness)';
+  else if (17 <= bmi && bmi <= 18.4) return 'Underweight (Mild thinness)';
+  else if (18.5 <= bmi && bmi <= 24.9) return 'Normal Range';
+  else if (25 <= bmi && bmi <= 29.9) return 'Overweight (Pre-obese)';
+  else if (30 <= bmi && bmi <= 34.9) return 'Obese (Class I)';
+  else if (25 <= bmi && bmi <= 39.9) return 'Obese (Class II)';
+
+  return 'Obese (Class III)';
 };
 
 try {
   const { weight, height } = parseArguments(process.argv);
-  bmiCalculator(weight, height);
+  console.log(bmiCalculator(weight, height));
 } catch (error: unknown) {
   let errorMessage = 'Something bad happened.';
   if (error instanceof Error) {
@@ -45,3 +44,5 @@ try {
   }
   console.log(errorMessage);
 }
+
+export {bmiCalculator};
--- part9/exercises/index.ts
+++ part9/exercises/index.ts
@@ -1,8 +1,31 @@
 import express = require('express');
+import { bmiCalculator } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (_req, res) => {
+  try {
+    let height: number = Number(_req.query.height);
+    let weight: number = Number(_req.query.weight);
+
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0)
+      throw new Error('malformatted parameters');
+
+    let bmi: string = bmiCalculator(weight, height);
+
+    res.send({
+      weight: weight,
+      height: height,
+      bmi: bmi,
+    });
+  } catch (error) {
+    res.send({
+      error: error.message,
+    });
+  }
 });
 
 const PORT = 3003;
