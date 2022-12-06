7bef8b81cb7788e259290ace0627415a9f4a5ed8
divyanshu0x16 <divyanshu22.dm@gmail.com>
2022-12-06 16:30:39 +0530

    completed exercise 9.9

--- part9/typing-express/patientor/backend/index.ts
+++ part9/typing-express/patientor/backend/index.ts
@@ -1,13 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+// app.get('/api/patients', (_req, res) => {
+//   console.log('patients get request');
+//   res.send('pong');
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
